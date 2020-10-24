32df6c4fc5e9b3fb7d0df7c41f28d440283003c4
Rupali@steinwegonline.com <Rupali@steinwegonline.com>
2020-10-24 22:28:28 +0800

    write js code to hide/show faqs

--- faq-accordion-card-main/code files/scripts.js
+++ faq-accordion-card-main/code files/scripts.js
@@ -1,7 +1,17 @@
 function toggleStates(id) {
-    /*
-    add clicked status if there is no clicked status to that id, transform arrow, show answer,change qs color to dark
-    remove clicked status if there is clicked status to that id, transform arrow, hide answer,change qs color to grey
-    */
-   
+   var element = document.getElementById(id);
+   var hasSelectedClass = element.classList.contains("selected");
+   if(hasSelectedClass) {
+        element.classList.remove("selected");
+        element.querySelector(".question").style.color="#4a4b5e";
+        element.querySelector(".question").style.fontWeight="400";
+        element.querySelector(".arrow").style.transform="rotate(0deg)";
+        element.nextElementSibling.style.display="none";
+   } else {
+        element.classList.add("selected");
+        element.querySelector(".question").style.color="#1d1e35";
+        element.querySelector(".question").style.fontWeight="700";
+        element.querySelector(".arrow").style.transform="rotate(180deg)";
+        element.nextElementSibling.style.display="block";
+    }
 }
