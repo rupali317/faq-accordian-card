8e9a0e6f3f9222f25e51bca4cd97bd16e4e3bfc9
Rupali@steinwegonline.com <Rupali@steinwegonline.com>
2020-10-24 21:43:36 +0800

    adding javascript files

---
+++ faq-accordion-card-main/code files/scripts.js
@@ -0,0 +1,7 @@
+function toggleStates(id) {
+    /*
+    add clicked status if there is no clicked status to that id, transform arrow, show answer,change qs color to dark
+    remove clicked status if there is clicked status to that id, transform arrow, hide answer,change qs color to grey
+    */
+   
+}
